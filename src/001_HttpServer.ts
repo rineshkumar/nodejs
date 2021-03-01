e4f6a7615534d16f789081a88cfaefd896f11b98
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-02 00:52:27 +1100

    exporing and using module

--- src/001_HttpServer.ts
+++ src/001_HttpServer.ts
@@ -2,5 +2,5 @@ const http = require("http");
 const math2 = require("./modules/Math2")
 http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World! '+math2.addNumbers(10,20));
+    res.end('Hello World! '+math2.multiplyNumbes(10,20));
   }).listen(8080);
--- src/modules/Math2.ts
+++ src/modules/Math2.ts
@@ -1,3 +1,12 @@
 exports.addNumbers=(a:number,b:number):number=>{
     return a + b;
 };
+exports.subractNumbers=(a:number,b:number):number=>{
+    return a - b;
+};
+exports.multiplyNumbes = (a: number, b: number) => {
+    return a * b;
+};
+exports.divideNumbes = (a: number, b: number) => {
+    return a / b;
+};
