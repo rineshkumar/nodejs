5c8a7df7ae1131a01fab3a841479b6f4e9b38da1
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-04 12:41:07 +1100

    asynchronous programming using callbacks.

---
+++ src/002_ReadFromFIle.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs'
+import { RSA_NO_PADDING } from 'node:constants'
+
+fs.readFile('001_HttpServer.js', (error,data)=>{
+    if(error) {
+        console.log(error);
+        return
+    };
+    console.log(data.toString());
+});
+console.log("Main program ended ")
+//
+
+//After compiling , execute it from inside the dist folder. 
+//node 002_ReadFromFile.js
+//Notice that on execution Main Program ended statement will be displayed first. 
+//Then the output of http server file will be displayed 
+//Reason behind this is asychronous programming using callback
