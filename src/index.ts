60413b0c8ea550d3c42c98b668318f6f9cdb2d59
Rinesh Kumar <rineshkumar@gmail.com>
2021-04-06 13:10:37 +1000

    Using the port environment variable

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import cors from 'cors'
 import helmet from 'helmet'
 dotenv.config();
 //environment variables. 
-
+if (!process.env.PORT) {
+    process.exit(1)
+}
+const PORT: number = parseInt(process.env.PORT as string, 10)
+const app = express()
 //Configurartion 
 
 //Server processing 
