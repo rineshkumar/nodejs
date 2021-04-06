799d6496f305f5384e317bfa3317f4d794aa7b2f
Rinesh Kumar <rineshkumar@gmail.com>
2021-04-06 13:43:51 +1000

    Adding data

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,29 @@
 //Data model interfaces 
-import{BaseItem,Item} from './item.interface'
-import {Items} from "./items.interface"
+import { BaseItem, Item } from './item.interface'
+import { Items } from "./items.interface"
 // In memory store 
-//Service interface 
+let item: Items = {
+    1: {
+        id: 1,
+        name: "Name1",
+        description: "desc1",
+        image: "https://cdn.auth0.com/blog/whatabyte/burger-sm.png",
+        price: 10
+    },
+    2: {
+        id: 2,
+        name: "Name2",
+        description: "desc2 ",
+        image: "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png",
+        price: 20
+    },
+    3: {
+        id: 3,
+        name: "Name3",
+        description: "desc3 ",
+        image: "https://cdn.auth0.com/blog/whatabyte/tea-sm.png",
+        price: 30
+    }
+
+};
+//Service interface
