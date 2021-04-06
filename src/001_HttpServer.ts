61f8175b26ee51401600f8a35ae75ed352bb23b4
Rinesh Kumar <rineshkumar@gmail.com>
2021-04-06 12:45:20 +1000

    Deleted All Previous Files

--- src/001_HttpServer.ts
+++
@@ -1,6 +0,0 @@
-import * as http from 'http'
-
-http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" })
-  .end("hello World 123");
-}).listen(8080);
--- src/003_Events.ts
+++
@@ -1,9 +0,0 @@
-//EventEmitter --> binding of events and event handlers, raising events 
-import * as events from 'events'
-const eventEmitter = new events.EventEmitter();
-//Binding events and event handler 
-eventEmitter.on('dataSourceConnected', ()=>{console.log("Data source connected ")})
-//Triggering event 
-eventEmitter.emit('dataSourceConnected');
-
-
