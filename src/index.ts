6c0fd01e55d9563b1bddda01b52f8574b23a1e1d
Rinesh Kumar <rineshkumar@gmail.com>
2021-04-06 13:10:21 +1000

    importing dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 //External modules 
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+dotenv.config();
 //environment variables. 
+
 //Configurartion 
+
 //Server processing 
